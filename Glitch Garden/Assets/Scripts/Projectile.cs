100f18a82c43d0f5dc1ae0c0976bf3cdf2fe59a6
salihsendil <sendillsalih@gmail.com>
2023-04-05 03:05:15 +0300

    compulsory commit
    +added health and damage system
    +added kill vfx
    +spawn mechanics for defenders
    +defenders snapping on the squares
    +added uı for select defenders which will spawn
    
    Note: unity has crashed so I pushed the commit.

--- Glitch Garden/Assets/Scripts/Projectile.cs
+++ Glitch Garden/Assets/Scripts/Projectile.cs
@@ -5,9 +5,22 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     [SerializeField] float speed = 8f;
+    [SerializeField] int damage = 50;
 
     void Update()
     {
         transform.Translate(Vector2.right * speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        var health = other.GetComponent<Health>();
+        var attacker = other.GetComponent<Attacker>();
+        if (health && attacker)
+        {
+            health.DealDamage(damage);
+            Destroy(gameObject);//destroying projectile after collision
+        }
+
     }
 }
